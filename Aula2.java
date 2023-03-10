423746aa364fb41ca0340b5a3ffecfbd3a255122
gaabrenno <gaabrenno@gmail.com>
2023-03-10 19:32:22 -0300

    ex até media

--- Aula2.java
+++ Aula2.java
@@ -50,8 +50,59 @@ public class Aula2 {
 
     }
 
+    public static void multa(){
+        int multas;
+
+        String lemultas = JOptionPane.showInputDialog("Insira a quantidade de multas em um ano:");
+        multas = Integer.parseInt(lemultas);
+
+        if (multas > 5){
+            System.out.print("Você até pode ser uma boa pessoa, mas não um bom motorista!");
+        } else if (multas == 5) {
+            System.out.print("Você dirige bem, mas precisa prestar mais atenção!");
+        } else {
+            System.out.print("Parabéns! Você é um bom motorista!");
+        }
+
+    }    public static void festa(){
+        int noite;
+
+        String lenoite = JOptionPane.showInputDialog("Insira a quantidade de amigos que você encontrou na festa:");
+        noite = Integer.parseInt(lenoite);
+
+        if (noite > 5){
+            System.out.print("Você é popular!");
+        } else if (noite == 5) {
+            System.out.print("Você é uma pessoa normal!");
+        } else {
+            System.out.print("Você precisa socializar mais!");
+        }
+
+    }
+
+    public static void media (){
+        float n1, n2, n3, n4, soma, media;
+        String aluno;
+
+        aluno = JOptionPane.showInputDialog("insira o nome do aluno");
+        String len1 = JOptionPane.showInputDialog("Insira a primeira nota do aluno:");
+        n1 = Integer.parseInt(len1);
+        String len2 = JOptionPane.showInputDialog("Insira a primeira nota do aluno:");
+        n2 = Integer.parseInt(len2);
+        String len3 = JOptionPane.showInputDialog("Insira a primeira nota do aluno:");
+        n3 = Integer.parseInt(len3);
+        String len4 = JOptionPane.showInputDialog("Insira a primeira nota do aluno:");
+        n4 = Integer.parseInt(len4);
+
+        soma = n1 + n2 + n3 + n4;
+        media = soma / 4;
+
+        System.out.print("A média do aluno "+aluno+" é: "+media);
+
+    }
+
     public static void main(String[] args) {
-        desconto();
+        media();
     }
 
 }
