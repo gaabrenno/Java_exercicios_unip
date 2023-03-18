f3ff56651945d69288fecff47e9511f1ac6e2aee
gaabrenno <gaabrenno@gmail.com>
2023-03-17 21:09:58 -0300

    somaSub ok

--- Aula4.java
+++ Aula4.java
@@ -4,15 +4,14 @@ import javax.swing.*;
 
 public class Aula4 {
 
-    public static void somaSub(){
-        int impr, soma = 0, nun;
-
-        for (nun=1256; nun <= 1278;nun++) {
-            nun = nun + nun;
-            soma = nun;
+    public static void somaSub() {
+        int soma = 0;
+        for (int nun = 1256; nun <= 1278; nun++) {
+            soma += nun;
         }
-        JOptionPane.showMessageDialog(null, "O resultado final é: "+soma);
+        JOptionPane.showMessageDialog(null, "O resultado final é: " + soma);
     }
+
 
 
     public static void main(String[]args){
