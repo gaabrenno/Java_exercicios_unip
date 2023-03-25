cfac1f3305e459d14b785b2de73d5bf1e79aedce
gaabrenno <gaabrenno@gmail.com>
2023-03-25 12:26:44 -0300

    resto

--- Aula4.java
+++ Aula4.java
@@ -12,17 +12,47 @@ public class Aula4 {
         JOptionPane.showMessageDialog(null, "O resultado final é: " + soma);
     }
 
-    public static void quadrado(){
+    public static void quadrado() {
         int nun, quadrado;
 
-        for (nun = 1; nun<=10; nun++){
-            quadrado = nun*nun;
-            JOptionPane.showMessageDialog(null,"O quadrado de "+nun+" é:"+quadrado);
+        for (nun = 1; nun <= 10; nun++) {
+            quadrado = nun * nun;
+            JOptionPane.showMessageDialog(null, "O quadrado de " + nun + " é:" + quadrado);
         }
     }
 
-    public static void main(String[]args){
+    public static void aplicacao() {
+        int periodo, contador = 1;
+        float valor, juros;
 
-        quadrado();
+        String leValor = JOptionPane.showInputDialog("Infome o valor a ser aplicado:");
+        String lePeriodo = JOptionPane.showInputDialog("Informe o periodo da aplicação:");
+
+        periodo = Integer.parseInt(lePeriodo);
+        valor = Integer.parseInt(leValor);
+        juros = (float) (valor * 0.1);
+
+        while (contador <= periodo) {
+            valor = valor + juros;
+            JOptionPane.showMessageDialog(null, contador + "º mês: " + valor);
+            juros = (float) (valor * 0.1);
+            contador++;
+        }
+    }
+
+    public static void resto() {
+        int nun, soma = 0;
+
+        for (nun = 160; nun <= 190; nun++) {
+            if (nun % 2 == 0) {
+                soma = nun + soma;
+                JOptionPane.showMessageDialog(null, "A soma dos números pares no intervalor entre 160 a 190 é: " + soma);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        resto();
     }
 }
