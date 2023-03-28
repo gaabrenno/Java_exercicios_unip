df7763d45b17ae572b352f84d151b7895d409fa4
gaabrenno <gaabrenno@gmail.com>
2023-03-27 21:23:45 -0300

    cursos e introdução ao caos

--- Aula5.java
+++ Aula5.java
@@ -1,4 +1,19 @@
 package lpoo.aula1;
-
+import javax.swing.*;
 public class Aula5 {
+
+    public static void cursos(){
+        String cursos[]= {"Ciencias da Computação","Engenharia Civil", "Educação Fisica", "Pisicologia"};
+
+        for (String i:cursos){
+            System.out.println(i);
+        }
+    }
+
+    public static void caos(){
+
+    }
+    public static void main(String[]args){
+        caos();
+    }
 }
